1a66c3609bf66d00bb1bd9fb10ff07c71bbb4d13
Jose Kalladanthyil <jk3111@esewsproj1-009.(none)>
2013-07-16 17:50:10 +0100

    nearly done!!! only need to update the count which is on the first line of the filenames text file and then refactor copied code!

--- dev/plugins/mesh_netcdf/generator.py
+++ dev/plugins/mesh_netcdf/generator.py
@@ -2,7 +2,6 @@
 
 import os
 import glob
-from test.test_geo import test_geo_files
 
 pwd = os.getcwd()
 test = pwd+"/../../tests"
@@ -91,12 +90,26 @@ os.system("python mesh_terminal -l BY -g "+test+"/testfileBY_1.geo --id "+idfile
 os.system("python mesh_terminal -l BY -g "+test+"/testfileBY_2.geo "+rtpmultdomain+" --mesh")
 os.system("python mesh_terminal -l BY -g "+test+"/testfileBY_3.geo --id "+idfile+" "+rtpmultdomain+"  --mesh")
 
-ref = open("test/ref.geo", 'r')
-test_file = open("test/test.geo", 'r')
-os.system("py.test"+ test_geo_files(ref, test_file))
-ref.close()
-test_file.close()
-  #os.system("py.test test/test_geo.py")
+
+
+#writes all the .geo filenames into the text file called filenames in test.
+fnames = glob.glob("/home/jk3111/test_engine/dev/tests/*.geo")
+filenames = open(pwd+"/test/filenames.txt", 'w')
+filenames.write("1\n")
+for i in fnames:
+  filenames.write(i+"\n")
+
+filenames.close()
+
+#prints out the current file teting and then diff it
+for n in range(0,len(fnames)):
+  filenames = open(pwd+"/test/filenames.txt", 'r')
+  count = int(names.readline())  
+  lines = filenames.readlines()
+  print lines[count]
+  filenames.close()
+  os.system("py.test test/test_geo.py")
+
 
 print  "Done Testing"
 
--- dev/plugins/mesh_netcdf/test/test_geo.py
+++ dev/plugins/mesh_netcdf/test/test_geo.py
@@ -3,27 +3,52 @@
 import pytest
 from support_files.diff_match_patch import diff_match_patch
 import glob
+import os
 
-class test_geo:
 
-  def test_geo_files(reference, test):
-    filenames = glob.glob("/home/jk3111/test_engine/dev/tests/*.geo")
-#  for i in filenames:
-    
-    diffcheck = diff_match_patch()
-    diffs = diffcheck.diff_main(reference.read(), test.read())
-    not_zeroes = [i for i, v in enumerate(diffs) if v[0] != 0]
-    print string
-    assert (not not_zeroes)
+def test_geo_files():
+  pwd = os.getcwd()
 
-  def updateParam(referring, tester):
-  	ref.close()
-  	test.close()
-  	global ref 
-  	ref = open(referring, 'r')
-  	global test  
-  	test = open(tester, 'r') 
-  	test_geo()
+  #opens the list of filenames in the folder
+  names = open("filenames.txt", 'r')
+  
+  #gets the count number on the file to be read
+  count = int(names.readline())
+  
+  lines = names.readlines()
+  fname = lines[count]
+  
+  #diff the two files
+  ref  = open(pwd+"/../../../tests/model_answers/"+fname,'r')
+  test = open(pwd+"/../../../tests/"+fname,'r')  
+  diffcheck = diff_match_patch()
+  diffs = diffcheck.diff_main(ref.read(), test.read())
+ 
+  test.close()
+  ref.close()
+  names.close()
+  
+  fnames = glob.glob("/home/jk3111/test_engine/dev/tests/*.geo")
+  filenames = open(pwd+"/test/filenames.txt", 'w')
+  filenames.write(str(count+1)+"\n")
+  for i in fnames:
+    filenames.write(i+"\n")
+
+  filenames.close()
+ 
+ 
+  not_zeroes = [i for i, v in enumerate(diffs) if v[0] != 0]
+  print string
+  assert (not not_zeroes)
+
+def updateParam(referring, tester):
+	ref.close()
+ 	test.close()
+ 	global ref 
+ 	ref = open(referring, 'r')
+ 	global test  
+ 	test = open(tester, 'r') 
+ 	test_geo()
 
 
   
