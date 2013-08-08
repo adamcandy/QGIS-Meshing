94cfe1b3b8b1e36f0f1f0d5a51671809e0d2c930
Magdalena Gocek <mg2111@esewsproj1-005.ese.ic.ac.uk>
2013-08-08 18:03:14 +0100

    Split it so that each item is tested right after its generation. It's not perfect, there are 2 main problems right now: 1. The whole thing only works the second time its tested (i.e. you have to do 'make test' twice for it to work) 2. I still need to figure out how to extract the test functions into one module.

---
+++ dev/tests/test_Compound_lines_0.py
@@ -0,0 +1,27 @@
+import os, sys, ntpath
+
+sys.path.append(os.path.abspath(os.path.dirname(__file__) + '/testing_modules/'))
+from file_generation import generate_files, make_directory
+
+sys.path.append(os.path.abspath(os.path.dirname(__file__) + '/testing_modules/'))
+
+from test_msh import mesh_file_test
+from test_geo import geo_files_test
+
+
+test = os.path.dirname(os.path.realpath(__file__)) + "/output"
+support_file_path = os.path.dirname(os.path.realpath(__file__)) + "/support"
+
+
+fname = "test_Compound_lines_0"
+command = "-l LY -g "+test+"/test_Compound_lines_0/test_Compound_lines_0.geo "+support_file_path+"/rtopo_shape_DN__2.shp --mesh"
+
+
+generate_files(fname, command)
+
+
+
+def test_annulus_bn_geo():
+	curr_file = os.path.dirname(os.path.realpath(__file__)) + "/output/" + fname + "/" + fname + ".geo"
+
+	assert geo_files_test(curr_file),"%s does not match the model answer" % (ntpath.basename(curr_file).rstrip())
---
+++ dev/tests/test_template.py
@@ -0,0 +1,39 @@
+import os, sys
+
+sys.path.append(os.path.abspath(os.path.dirname(__file__) + '/testing_modules/'))
+from helper_module import generate_files, make_directory
+
+sys.path.append(os.path.abspath(os.path.dirname(__file__) + '/testing_modules/'))
+
+from test_msh import mesh_file_test
+from test_geo import geo_files_test
+
+
+test = os.path.dirname(os.path.realpath(__file__)) + "/output"
+support_file_path = os.path.dirname(os.path.realpath(__file__)) + "/support"
+
+
+########################### APPLY YOUR CHANGES HERE: ##########################
+
+fname = "name_of_the_file" # just the name, no forward or backslashes!
+command = 	"command_for_generating_geo_and_msh_files" # see modular_meshing.py for help
+
+###############################################################################
+
+generate_files(fname, command)
+
+
+
+def test_annulus_bn_geo():
+	curr_file = os.path.dirname(os.path.realpath(__file__)) + "/output/" + fname + "/" + fname + ".geo"
+
+	assert geo_files_test(curr_file),"%s does not match the model answer" % (ntpath.basename(curr_file).rstrip())
+
+
+def test_annulus_bn_msh():
+	curr_file = os.path.dirname(os.path.realpath(__file__)) + "/output/" + fname + "/" + fname + ".msh"
+
+    assert mesh_file_test(curr_file),"%s does not match the model answer" % (ntpath.basename(curr_file).rstrip())
+
+
+############################# ADD MORE TESTS HERE: ############################
