097974697f095ab1267b48b26bd1ef997aa9728a
Magdalena Gocek <mg2111@imperial.ac.uk>
2013-09-18 14:19:57 +0100

    Fixed a bug in PreMeshingFunctions.py

--- plugins/mesh_surface/scripts/PreMeshingFunctions.py
+++ plugins/mesh_surface/scripts/PreMeshingFunctions.py
@@ -1,42 +1,42 @@
 
 ##########################################################################
-#  
+#
 #  QGIS-meshing plugins.
-#  
+#
 #  Copyright (C) 2012-2013 Imperial College London and others.
-#  
+#
 #  Please see the AUTHORS file in the main source directory for a
 #  full list of copyright holders.
-#  
+#
 #  Dr Adam S. Candy, adam.candy@imperial.ac.uk
 #  Applied Modelling and Computation Group
 #  Department of Earth Science and Engineering
 #  Imperial College London
-#  
+#
 #  This library is free software; you can redistribute it and/or
 #  modify it under the terms of the GNU Lesser General Public
 #  License as published by the Free Software Foundation,
 #  version 2.1 of the License.
-#  
+#
 #  This library is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 #  Lesser General Public License for more details.
-#  
+#
 #  You should have received a copy of the GNU Lesser General Public
 #  License along with this library; if not, write to the Free Software
 #  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
 #  USA
-#  
+#
 ##########################################################################
 
 """
 This class includes all of the functions that used by MeshOperations.py for the creation of the mesh.
 """
 
-import sys
-sys.path.append("../")
-from mesh_surface.StandardModules import *
+import sys, os
+sys.path.append(os.path.abspath(os.path.dirname(__file__) + '/..'))
+from StandardModules import *
 from PyQt4.QtGui import *
 from export_geo import *
 
@@ -66,7 +66,7 @@ class PreMesh(DefineDomain):
 	"""
 	def convertShape(self):
 		getGeoFile(str(self.domainShapefileLayerFileName), str(self.domainShapefileLayerFileName[:-4]))
-		self.geoFileName = '%s.geo' % self.domainShapefileLayerFileName[:-4]		
+		self.geoFileName = '%s.geo' % self.domainShapefileLayerFileName[:-4]
 
 	def define_bounds(self, ok):
 		DefineDomain.define_bounds(self, ok)
@@ -85,9 +85,9 @@ class PreMesh(DefineDomain):
 
 		idText = self.dlg.ui.IdDropdown.currentText()
 		idIndex = self.dlg.ui.IdDropdown.findText(idText)
-		self.idFilePath = self.dlg.ui.IdDropdown.itemData(idIndex).toString() 
-	
-		self.threshold = 0.0 
+		self.idFilePath = self.dlg.ui.IdDropdown.itemData(idIndex).toString()
+
+		self.threshold = 0.0
 
 		if self.dlg.ui.define_th.isChecked():
 			self.threshold = float(str(self.dlg.ui.Threshold.text()))
@@ -116,10 +116,10 @@ class PreMesh(DefineDomain):
 			self.runIdDef()
 			self.geoFileName = '%s.geo' % self.domainSavePath
 
-	
+
 	"""
-	Not in use. This functionality is now possible within RasterCalc. 
-	Performed the calculation of the minimum of multiple NetCDF files using grdmath and imported the resulting file into QGIS 
+	Not in use. This functionality is now possible within RasterCalc.
+	Performed the calculation of the minimum of multiple NetCDF files using grdmath and imported the resulting file into QGIS
 	in pseudolcolour.
 	"""
 	def calculateMinimum(self):
@@ -136,7 +136,7 @@ class PreMesh(DefineDomain):
 				# Min of overlapping regions.
 				call (["/usr/lib/gmt/bin/grdmath", str(list(self.activeNetCDFs)[i][1].toString()), str(list(self.activeNetCDFs)[i + 1][1].toString()) \
 				, "MIN", "=", "/tmp/tmp.tif"])
-		
+
 			# After the first iteration we want to use the newly created tmp file and the next layer down.
 			if i > 0 and i < range(len(list(self.activeNetCDFs)) - 1)[-1]:
 				# Min of the newly created tmp and the next layer.
